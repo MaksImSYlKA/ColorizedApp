a3016781f5f73286004a95e2606cd7bc6adeb453
Maxim Sylka <msylka@me.com>
2023-11-19 21:25:06 +0300

    Create conreolts and their outlets

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var coloredView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLabelsOfSlider()
+        coloredView.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func ValueOfSliderChanges(_ sender: Any) {
+        let currentColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+     
+        coloredView.backgroundColor = currentColor
+        setupLabelsOfSlider()
+    }
+    
+  
+    private func setupLabelsOfSlider() {
+        redValueLabel.text = String(format: "%.2f" ,redSlider.value)
+        greenValueLabel.text = String(format: "%.2f" ,greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f" ,blueSlider.value)
+    }
+    
 }
 
