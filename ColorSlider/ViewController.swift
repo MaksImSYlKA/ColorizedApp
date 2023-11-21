56e927e78f05966fffe8e2eabd512e335a488246
Maxim Sylka <msylka@me.com>
2023-11-21 07:27:56 +0300

    Split  methods  for change labels value and setup color for view

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -8,42 +8,50 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     // MARK: - IB Outlets
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
-
+    
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
     
     @IBOutlet var coloredView: UIView!
+    
+    //MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        SetupTextAndColorForControls()
+        setupColorOfView()
         coloredView.layer.cornerRadius = 15
     }
+    //MARK: - IB Actions
     
-    @IBAction func ChangeValueOfSlider(_ sender: Any) {
-        SetupTextAndColorForControls()
+    @IBAction func changeValueOfSlider(_ sender: UISlider) {
+        changeValueOfLabel(sender)
+        setupColorOfView()
     }
-    
-    private func SetupTextAndColorForControls() {
-        let currentColor = UIColor(
+    //MARK: - Private Methods
+    private func setupColorOfView() {
+        coloredView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
-        )
-        
-        coloredView.backgroundColor = currentColor
-        
-        redValueLabel.text = String(format: "%.2f" ,redSlider.value)
-        greenValueLabel.text = String(format: "%.2f" ,greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f" ,blueSlider.value)
-        
+            )
     }
-    
+    private func changeValueOfLabel (_ sender: UISlider) {
+        switch sender {
+            case redSlider:
+                redValueLabel.text = String(format: "%.2f" ,redSlider.value)
+            case greenSlider:
+                greenValueLabel.text = String(format: "%.2f" ,greenSlider.value)
+            case blueSlider:
+                blueValueLabel.text = String(format: "%.2f" ,blueSlider.value)
+            default:
+                break
+        }
+    }
 }
 
